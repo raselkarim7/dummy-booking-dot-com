234161bba972886020be315229719a19c006436e
Rasel Karim <raselkarim7@gmail.com>
2021-03-07 12:45:23 +0600

    Step 2: html 5 datepicker set, as using no css library

--- resources/react/src/components/ReusableSearch.js
+++ resources/react/src/components/ReusableSearch.js
@@ -23,8 +23,15 @@ const ReusableSearch = () => {
                 <p>Treat yourself. Your Dream resort stay in just  a few clicks away</p>
             </div>
             <div className="search-field-wrapper"> 
-                <div> <span> <i class="fas fa-car"></i>  More places than you can ever go (but you can try). </span> </div>
-                <div> <span> <i class="far fa-calendar-alt"></i> Check In </span> <span> - </span> <span> Check Out </span> </div>
+                <div> 
+                    <span className="full-width search-first-input"> <i class="fas fa-car"></i> <input className="border-less-input" placeholder="More places than you can ever go (but you can try). Search here." />  </span> 
+                </div>
+
+                {/* <div> <span> <i class="far fa-calendar-alt"></i> Check In </span> <span> - </span> <span> Check Out </span> </div> */}
+
+                <div className="date-range"> 
+                    <span> <input type="date" /> </span> <span> - </span> <span> <input type="date" />  </span> 
+                </div>
                 <div> <i class="fas fa-user"></i> <span>Two Adults. </span> <span>0 Children .</span> <span> 1  room </span> <i class="fas fa-arrows-alt-v"></i> </div>
                 <div> <h2> Search </h2> </div>
             </div>
